b7e38323c31b371ea27c0eca12f629937c1442c1
Wolney Oliveira <wolney.js@outlook.com>
2025-07-28 20:22:14 -0300

    build: project setup

---
+++ config/eslint-config/base.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+  },
+}
