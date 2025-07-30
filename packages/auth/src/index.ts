541c0bbc8bfb007b1f1715fd248839562025d7ed
Wolney Oliveira <wolney.js@outlook.com>
2025-07-29 22:57:01 -0300

    feat: setup casl for auth package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,43 @@
+import {
+  AbilityBuilder,
+  type CreateAbility,
+  createMongoAbility,
+  type MongoAbility,
+} from '@casl/ability'
+
+import { z } from 'zod'
+import type { User } from './models/user'
+import { permissions } from './permissions'
+import { projectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
+
+const appAbilitiesSchema = z.union([
+  userSubject,
+  projectSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,19 @@
+import type { AbilityBuilder } from '@casl/ability'
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+import type { Role } from './roles'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN(_, { can }) {
+    can('manage', 'all')
+  },
+  MEMBER(_, { can }) {
+    can('manage', 'Project')
+  },
+  BILLING() {},
+}
